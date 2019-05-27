2ca07a33f8cd5bcfd7f41bbd0e1f12a17bbcb166
Ivelina <ivs.yordanova@gmail.com>
2019-05-27 10:29:57 +0100

    refactor to use hooks

--- src/index.js
+++ src/index.js
@@ -1,57 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
+import useLocation from './useLocation';
 
-class App extends React.Component {
-    state = { lat: null, errorMessage: '' };
+const App = () => {
+    const [lat, errorMessage] = useLocation();
 
-//    always call super first
-//    constructor(props) {
-//        super(props);
-//    }
-
-    // avoid having multiple returns in "render" - use helper methods
-    renderContent() {
-        if(this.state.errorMessage && !this.state.lat) {
-            return <div>Error: { this.state.errorMessage }</div>
-        }
-        if(!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay  lat={ this.state.lat }/>
-        }
-        return <Spinner message="Please accept location request"/>
+    let content;
+    if(errorMessage) {
+        content = <div>Error: { errorMessage }</div>
+    } else if( lat) {
+        content = <SeasonDisplay  lat={ lat }/>
+    } else {
+        content = <Spinner message="Please accept location request"/>;
     }
-
-
-    render () {
-        return (
-            <div>
-                { this.renderContent() }
-            </div>
-        );
-    }
-
-    componentDidMount() {
-        // data loading potentially
-       window.navigator.geolocation.getCurrentPosition(
-           (position) => this.setState({ lat: position.coords.latitude }),
-           (error) => this.setState({ errorMessage: error.message })
-       );
-    }
-
-    componentDidUpdate() {
-        //right after render()
-        console.log('Component did update');
-    }
-
-    componentWillUnmount() {
-        console.log('Component will unmount');
-    }
-
-//    almost never used
-//    shouldComponentUpdate()
-//    getDerivedStateFromProps()
-//    getSnapshotBeforeUpdate()
+    return <div className="border red">{ content }</div>;
 };
 
 ReactDOM.render(
